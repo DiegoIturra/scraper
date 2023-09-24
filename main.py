e40eb90d0df74f3b9ec046fdc3ec5584e10a706e
Diego Iturra <diturra@dparadig.com>
2023-09-24 19:52:23 -0300

    feat: create a new class that manage the list of urls and remove a incorrect data type declaration in functionprocess_wishlist

--- main.py
+++ main.py
@@ -27,12 +27,66 @@ class Utils:
         return price
 
 
-class Scraper:
+class DataCollector:
+    def __init__(self):
+        self.data = []
+
+    def add_data(self, data):
+        self.data.append(data)
+
+    def get_data(self):
+        return self.data
+    
+    def print_all_data(self):
+        for data in self.data:
+            print(data)
+            print()
+
+class WishlistManager:
 
     def __init__(self):
-        pass
-        # TODO: change to a list of urls
+        self.wishlist = [
+            'https://www.buscalibre.cl/v2/pendientes_486712_l.html',
+            'https://www.buscalibre.cl/v2/daredevil-mark-waid_1520599_l.html'
+            'https://www.buscalibre.cl/v2/software-development_503034_l.html',
+            'https://www.buscalibre.cl/v2/comics_545021_l.html',
+            'https://www.buscalibre.cl/v2/pendientes-2_567662_l.html',
+            'https://www.buscalibre.cl/v2/historia_606807_l.html',
+            'https://www.buscalibre.cl/v2/feminismo_624086_l.html',
+            'https://www.buscalibre.cl/v2/pendientes-3_657656_l.html',
+            'https://www.buscalibre.cl/v2/latinoamericana_711603_l.html',
+            'https://www.buscalibre.cl/v2/estudio_727965_l.html',
+            'https://www.buscalibre.cl/v2/literatura_729752_l.html',
+            'https://www.buscalibre.cl/v2/contra_737670_l.html',
+            'https://www.buscalibre.cl/v2/english-books_816771_l.html',
+            'https://www.buscalibre.cl/v2/filosofia-sociologia_830510_l.html',
+            'https://www.buscalibre.cl/v2/cortos_831079_l.html',
+            'https://www.buscalibre.cl/v2/tolkien_921085_l.html',
+            'https://www.buscalibre.cl/v2/fullmetal_1003773_l.html',
+            'https://www.buscalibre.cl/v2/cuentos_1079781_l.html',
+            'https://www.buscalibre.cl/v2/estudio2_1218140_l.html',
+            'https://www.buscalibre.cl/v2/emprendimientos-y-finanzas_1450796_l.html',
+            'https://www.buscalibre.cl/v2/literatura-2_1469636_l.html',
+            'https://www.buscalibre.cl/v2/gantz_1488040_l.html',
+            'https://www.buscalibre.cl/v2/manga_1488300_l.html',
+            'https://www.buscalibre.cl/v2/chile_1536612_l.html',
+        ]
 
+    
+    def add_url(self, url: str):
+        self.wishlist.append(url)
+
+    def remove_url(self, url: str):
+        self.wishlist.remove(url)
+
+    def get_wishlist(self):
+        return self.wishlist
+    
+
+
+
+
+class Scraper:
 
     def get_books_url_from_wishlist(self, wishlist_url):
         """
@@ -117,53 +171,31 @@ class Scraper:
     def get_book_availability(self, price):
         return True if price else False
 
+ 
 
-def process_wishlist(wishlist_url: List[str], scraper: Scraper):
+def process_wishlist(wishlist_url: str, scraper: Scraper, data_collector: DataCollector):
     books_urls = scraper.get_books_url_from_wishlist(wishlist_url)
     wishlist_name = Utils.extract_name_from_url(wishlist_url)
 
     for book_url in books_urls:
         data = scraper.get_book_data_from_url(book_url)
         data = scraper.add_wishlist_name_to_data(data, wishlist_name)
-        pprint(data)
-        print()
+        data_collector.add_data(data)
 
 if __name__ == '__main__':
     scraper = Scraper()
+    data_collector = DataCollector()
+    wishlist_manager = WishlistManager()
 
-    wishlist = [
-        'https://www.buscalibre.cl/v2/pendientes_486712_l.html',
-        'https://www.buscalibre.cl/v2/daredevil-mark-waid_1520599_l.html'
-        'https://www.buscalibre.cl/v2/software-development_503034_l.html',
-        'https://www.buscalibre.cl/v2/comics_545021_l.html',
-        'https://www.buscalibre.cl/v2/pendientes-2_567662_l.html',
-        'https://www.buscalibre.cl/v2/historia_606807_l.html',
-        'https://www.buscalibre.cl/v2/feminismo_624086_l.html',
-        'https://www.buscalibre.cl/v2/pendientes-3_657656_l.html',
-        'https://www.buscalibre.cl/v2/latinoamericana_711603_l.html',
-        'https://www.buscalibre.cl/v2/estudio_727965_l.html',
-        'https://www.buscalibre.cl/v2/literatura_729752_l.html',
-        'https://www.buscalibre.cl/v2/contra_737670_l.html',
-        'https://www.buscalibre.cl/v2/english-books_816771_l.html',
-        'https://www.buscalibre.cl/v2/filosofia-sociologia_830510_l.html',
-        'https://www.buscalibre.cl/v2/cortos_831079_l.html',
-        'https://www.buscalibre.cl/v2/tolkien_921085_l.html',
-        'https://www.buscalibre.cl/v2/fullmetal_1003773_l.html',
-        'https://www.buscalibre.cl/v2/cuentos_1079781_l.html',
-        'https://www.buscalibre.cl/v2/estudio2_1218140_l.html',
-        'https://www.buscalibre.cl/v2/emprendimientos-y-finanzas_1450796_l.html',
-        'https://www.buscalibre.cl/v2/literatura-2_1469636_l.html',
-        'https://www.buscalibre.cl/v2/gantz_1488040_l.html',
-        'https://www.buscalibre.cl/v2/manga_1488300_l.html',
-        'https://www.buscalibre.cl/v2/chile_1536612_l.html',
-    ]
+    wishlist = wishlist_manager.get_wishlist()
 
     start_time = time()
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [executor.submit(process_wishlist, url, scraper) for url in wishlist]
+        futures = [executor.submit(process_wishlist, url, scraper, data_collector) for url in wishlist]
         concurrent.futures.wait(futures)
 
+    data_collector.print_all_data()
 
     final_time = time()
 
